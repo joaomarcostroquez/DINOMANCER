b22e7ee0ca9179ea0f029ae2a5f2179ba48038e7
Trqz <joaomarcostroquez@gmail.com>
2023-05-03 22:43:48 +0100

    programmed PlayerGunSelector

--- Assets/Scripts/Player/Shooting/PlayerGunSelector.cs
+++ Assets/Scripts/Player/Shooting/PlayerGunSelector.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class PlayerGunSelector : MonoBehaviour
 {
-    [SerializeField] private GunType gun;
+    [SerializeField] private GunType _gunType;
     [SerializeField] private Transform gunParent;
     [SerializeField] private List<GunScriptableObject> guns;
     //[SerializeField] PlayerIK inverseKinematics;
@@ -11,4 +12,18 @@ public class PlayerGunSelector : MonoBehaviour
     [Space]
     [Header("Runtime Filled")]
     public GunScriptableObject activeGun;
+
+    private void Start()
+    {
+        GunScriptableObject gun = guns.Find(gun => gun.type == _gunType);
+
+        if(gun == null)
+        {
+            Debug.LogError($"No GunScriptableObject found for GunType: {gun}");
+            return;
+        }
+
+        activeGun = gun;
+        gun.Spawn(gunParent, this);
+    }
 }

53d11592eac8bc329e056dc1e0b440681e56de31
Trqz <joaomarcostroquez@gmail.com>
2023-05-10 21:02:49 +0100

    navmesh package imported

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     //Common functionalities for all enemies
-    
+    protected NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
 }
--- Assets/Scripts/Enemies/RushingEnemy.cs
+++ Assets/Scripts/Enemies/RushingEnemy.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class RushingEnemy : Enemy
 {
-    [SerializeField] private bool rushingEnemy;
+    
 }

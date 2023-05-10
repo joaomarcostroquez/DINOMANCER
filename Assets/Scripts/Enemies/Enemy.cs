d2d32bfe36266a6be4cae1229a6406723e934fdf
Trqz <joaomarcostroquez@gmail.com>
2023-05-10 21:36:32 +0100

    programming RushingEnemy

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -7,9 +7,22 @@ public class Enemy : MonoBehaviour
 {
     //Common functionalities for all enemies
     protected NavMeshAgent navMeshAgent;
+    protected GameObject player;
+    [SerializeField] private float recalculateRouteToPlayerThreshold = 1f;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    //Moves in the direction of player until it is on sight
+    protected bool GetPlayerOnSight()
+    {
+        if(Physics.Raycast)
+        if(Vector3.Distance(player.transform.position, navMeshAgent.destination) > recalculateRouteToPlayerThreshold)
+        {
+            navMeshAgent.SetDestination(player.transform.position);
+        }
     }
 }

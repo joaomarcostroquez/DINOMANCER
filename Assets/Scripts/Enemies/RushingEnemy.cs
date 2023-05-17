d2a361b0ce6ce2ef17d3d136edc6d28347b8e9f2
Trqz <joaomarcostroquez@gmail.com>
2023-05-17 12:31:08 +0100

    programming RushingEnemy

--- Assets/Scripts/Enemies/RushingEnemy.cs
+++ Assets/Scripts/Enemies/RushingEnemy.cs
@@ -2,10 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class RushingEnemy : Enemy
 {
+    [SerializeField] private float rushAcceleration = 50f;
+
+    private Rigidbody _rigidbody;
+    private bool isRushing = false;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.isKinematic = true;
+    }
+
     private void Update()
     {
-        if(MoveUntilPlayerInRangeAndOnSight());
+        if (!isRushing)
+        {
+            if (MoveUntilPlayerInRangeAndOnSight())
+                StartRush();
+        } 
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRushing)
+        {
+            RushAttack();
+        }
+    }
+
+    private void StartRush()
+    {
+        navMeshAgent.isStopped = true;
+        navMeshAgent.enabled = false;
+
+        transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+        _rigidbody.isKinematic = false;
+        isRushing = true;
+    }
+
+    private void RushAttack()
+    {
+        _rigidbody.AddForce(transform.forward * rushAcceleration, ForceMode.Acceleration);
     }
 }

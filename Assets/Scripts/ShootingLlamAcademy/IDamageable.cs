16040ba64f1658368f148c8e67b93f82735e8dde
Trqz <joaomarcostroquez@gmail.com>
2023-06-03 11:06:15 +0100

    programming IDamageable based on LlamAcademy's tutorial

---
+++ Assets/Scripts/ShootingLlamAcademy/IDamageable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable
+{
+    public int currentHealth { get; }
+    public int maxHealth { get; }
+
+    public delegate void TakeDamageEvent(int damage);
+    public event TakeDamageEvent OnTakeDamage;
+
+    public delegate void DeathEvent(Vector3 position);
+    public event DeathEvent OnDeath;
+
+    public void TakeDamage(int damage);
+}

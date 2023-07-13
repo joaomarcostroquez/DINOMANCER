a780b683760cce470ed9865754081a6ad4e9d54a
Trqz <joaomarcostroquez@gmail.com>
2023-07-13 02:11:21 +0100

    changed healthPack model and animations

--- Assets/Scripts/Utility/LinearMovementRotation.cs
+++ Assets/Scripts/Utility/LinearMovementRotation.cs
@@ -7,6 +7,11 @@ public class LinearMovementRotation : MonoBehaviour
     [SerializeField] private Vector3 linearMovement, linearRotation;
     [SerializeField] private bool randomRotationDirection = false;
 
+    [Tooltip("If 0, will use the default frame rate.")]
+    [SerializeField] private int frameRate = 0;
+
+    private float timeCounter = 0;
+
     private int rotationDirection = 1;
 
     private void Start()
@@ -22,7 +27,22 @@ public class LinearMovementRotation : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(linearMovement * Time.deltaTime);
-        transform.Rotate(linearRotation * Time.deltaTime * rotationDirection);
+        if (frameRate == 0)
+        {
+            transform.Translate(linearMovement * Time.deltaTime);
+            transform.Rotate(linearRotation * Time.deltaTime * rotationDirection);
+        }
+        else
+        {
+            timeCounter += Time.deltaTime;
+            
+            if (timeCounter >= 1f / frameRate)
+            {
+                transform.Translate(linearMovement * timeCounter);
+                transform.Rotate(linearRotation * timeCounter * rotationDirection);
+                timeCounter = 0;
+            }
+        }
+        
     }
 }
